8f096c827d64d1fcd3ab18642a4176204177356f
Saha <PAYAL_K_SAHA@homedepot.com>
2017-12-05 12:03:54 -0500

    test update

--- integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
@@ -40,7 +40,6 @@ public class ApplicationServer {
             .put("EUREKA_CLIENT_ENABLED", "false")
             .put("RIBBON_EUREKA_ENABLED", "false")
             .put("REGISTRATION_SERVER_RIBBON_LISTOFSERVERS", "http://localhost:8883")
-          //  .put("REGISTRATION_SERVER_ENDPOINT", "http://localhost:8883")
             .build()
         );
     }
@@ -56,7 +55,7 @@ public class ApplicationServer {
 
     private static void waitUntilServerIsUp(String port) throws InterruptedException {
         HttpClient httpClient = new HttpClient();
-        int timeout = 120;
+        int timeout = 420;
         Instant start = Instant.now();
         boolean isUp = false;
 
